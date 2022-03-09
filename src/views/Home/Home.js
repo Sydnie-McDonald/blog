0251e6053bed57c66d1ac9d8ea3a9b5c719cda70
Sydnie McDonald <sydnie.r.mcdonald@gmail.com>
2022-03-09 16:50:07 -0600

    home.js

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -7,12 +7,22 @@ import './Home.css';
 
 export default function Home() {
   // add useState calls here for title, subtitle, font, align, and text
+  const [title, setTitle] = useState('testTitle');
+  const [subTitle, setSubTitle] = useState('testSubTitle');
+  const [font, setFont] = useState('testFont');
+  const [align, setAlign] = useState('testAlign');
+  const [text, setText] = useState('setText');
 
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
-      <Preview />
-      <Editor />
+      <Preview title={title} subtitle={subtitle} text={text} align={align} font={font} />
+      <Editor
+        setTitle={setTitle}
+        setSubTitle={setSubTitle}
+        setFont={setFont}
+        setAlign={setAlign}
+        setText={setText} />
     </main>
   );
 }
