b5106db96c06bcd000af936ae3cfbc11c18af3b9
Sydnie McDonald <sydnie.r.mcdonald@gmail.com>
2022-03-09 18:03:43 -0600

    fixed issues. Blog is working and the text updates

--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 import './Preview.css';
 
-export default function Preview({ title, subtitle, font, align, text }) {
+export default function Preview({ title, subTitle, font, align, text }) {
   return (
     <div className={`preview ${font}`} style={{ textAlign: align }}>
       <h1> {title}</h1>
-      <h3> {subtitle}</h3>
+      <h3> {subTitle}</h3>
       <p>{text}</p>
     </div>
   );
